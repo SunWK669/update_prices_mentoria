e844ed6a0fa810f15590e3b9698b74fdcc91312c
jobfy-br <jobfy@jobfybrasil.com>
2021-12-21 15:44:10 -0300

    feat: pega preco alexa
    atualiza requirements

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
+
+def main():
+    url = "https://www.zoom.com.br/smart-speaker/smart-speaker-amazon-echo-dot-alexa?_lc=88&searchterm=alexa"
+    session = requests.Session()
+    response = session.get(url)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    price_tags = soup.find("a", attrs={"data-testid": "value-anchor"})
+    print(price_tags.text)
+
+main()
